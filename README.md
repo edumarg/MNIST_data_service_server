e3aab764ed3a0351626ab1bc5212c8f0eaf8d1b2
Eduardo Margulis <edumarg@gmx.com>
2022-06-08 09:43:19 +0300

    base app using flask

---
+++ README.md
@@ -0,0 +1,1 @@
+"# MNIST_data_service_server" 
---
+++ main.py
@@ -0,0 +1,23 @@
+from mnist import MNIST
+from flask import Flask, json, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+mndata = MNIST('samples')
+images, labels = mndata.load_training()
+
+
+@app.route("/images", methods={"GET"})
+def getImages():
+    send_images = []
+    number = int(request.args.get("number", default=10))
+    for i in range(number):
+        send_images.append(images[i])
+    response = app.response_class(response=json.dumps(send_images), status=200, mimetype="application/json")
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
